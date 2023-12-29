8f98c70b49402099966c14c57bacd0ccc5d875a0
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-12-29 20:05:12 +0800

    adding routes

--- app.py
+++ app.py
@@ -24,6 +24,23 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credentials.json'
 
 translateClient = translate.Client()
 
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///translate'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
 # detect the language
 detectResponse = translateClient.detect_language('muraho nshuti yanjye')
 
@@ -36,120 +53,105 @@ translateResponse = translateClient.translate('muraho nshuti yanjye', 'en')
 print(translateResponse)
 # {'translatedText': 'hello my friend', 'detectedSourceLanguage': 'rw', 'input': 'muraho nshuti yanjye'}
 
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
 
 
+def do_login(user):
+    """Log in user."""
 
-# CURR_USER_KEY = "curr_user"
-
-# app = Flask(__name__)
-
-# # Get DB_URI from environ variable (useful for production/testing) or,
-# # if not set there, use development local db.
-# app.config['SQLALCHEMY_DATABASE_URI'] = (
-#     os.environ.get('DATABASE_URL', 'postgresql:///translate'))
-
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = False
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-# toolbar = DebugToolbarExtension(app)
-
-# connect_db(app)
-
-# @app.before_request
-# def add_user_to_g():
-#     """If we're logged in, add curr user to Flask global."""
-
-#     if CURR_USER_KEY in session:
-#         g.user = User.query.get(session[CURR_USER_KEY])
-
-#     else:
-#         g.user = None
+    session[CURR_USER_KEY] = user.id
 
 
-# def do_login(user):
-#     """Log in user."""
+def do_logout():
+    """Logout user."""
 
-#     session[CURR_USER_KEY] = user.id
-
-
-# def do_logout():
-#     """Logout user."""
-
-#     if CURR_USER_KEY in session:
-#         del session[CURR_USER_KEY]
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
 
 
-# @app.route('/signup', methods=["GET", "POST"])
-# def signup():
-#     """Handle user signup.
-
-#     Create new user and add to DB. Redirect to home page.
-
-#     If form not valid, present form.
-
-#     If the there already is a user with that email: flash message
-#     and re-present form.
-#     """
-#     if CURR_USER_KEY in session:
-#         del session[CURR_USER_KEY]
-#     form = UserAddForm()
-
-#     if form.validate_on_submit():
-#         try:
-#             user = User.signup(
-#                 password=form.password.data,
-#                 email=form.email.data,
-#             )
-#             db.session.commit()
-
-#         except IntegrityError as e:
-#             flash("Email already taken", 'danger')
-#             return render_template('signup.html', form=form)
-
-#         do_login(user)
-
-#         return redirect("/")
-
-#     else:
-#         return render_template('signup.html', form=form)
+@app.route('/')
+def home():
+    """render home page"""
+    return render_template('home.html')
 
 
-# @app.route('/login', methods=["GET", "POST"])
-# def login():
-#     """Handle user login."""
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
 
-#     form = LoginForm()
+    Create new user and add to DB. Redirect to home page.
 
-#     if form.validate_on_submit():
-#         user = User.authenticate(form.email.data,
-#                                  form.password.data)
+    If form not valid, present form.
 
-#         if user:
-#             do_login(user)
-#             return redirect("/")
+    If the there already is a user with that email: flash message
+    and re-present form.
+    """
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+    form = UserForm()
 
-#         flash("Invalid credentials.", 'danger')
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                password=form.password.data,
+                email=form.email.data,
+            )
+            db.session.commit()
 
-#     return render_template('login.html', form=form)
+        except IntegrityError as e:
+            flash("Email already taken", 'danger')
+            return render_template('signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('signup.html', form=form)
 
 
-# @app.route('/logout')
-# def logout():
-#     """Handle logout of user."""
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
 
-#     do_logout()
+    form = UserForm()
 
-#     flash("You have successfully logged out.", 'success')
-#     return redirect("/login")
+    if form.validate_on_submit():
+        user = User.authenticate(form.email.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('login.html', form=form)
 
 
-# @app.after_request
-# def add_header(req):
-#     """Add non-caching headers on every request."""
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
 
-#     req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     req.headers["Pragma"] = "no-cache"
-#     req.headers["Expires"] = "0"
-#     req.headers['Cache-Control'] = 'public, max-age=0'
-#     return req
+    do_logout()
+
+    flash("You have successfully logged out.", 'success')
+    return redirect("/login")
+
+@app.after_request
+def add_header(req):
+    """Add non-caching headers on every request."""
+
+    req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    req.headers["Pragma"] = "no-cache"
+    req.headers["Expires"] = "0"
+    req.headers['Cache-Control'] = 'public, max-age=0'
+    return req
