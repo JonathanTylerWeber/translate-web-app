b27ff20cdf636d07c3b194006c4111bd24feefa9
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-01-15 22:11:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 
 # toolbar = DebugToolbarExtension(app)
 
-with app.app_context() 
+with app.app_context():
     connect_db(app)
     db.create_all()
 
