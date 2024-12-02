4c0223599486895fc3450eed2efae0c4af131e3a
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-12-02 22:54:15 +0800

    mock res

--- app.py
+++ app.py
@@ -132,65 +132,74 @@ def health():
 
 @app.route('/translate', methods=["POST"])
 def translate():
-    """translate input"""
-    print("start translate")
-    if not g.user:
-        print("bad user")
-        flash("Must sign in first", "danger")
-        return redirect("/signup")
+    mock_response = {
+        'translation': '你好',
+        'pinyin': 'nǐ hǎo'
+    }
+    return jsonify(mock_response)
 
-    try:
-        print("start try")
-        if request.is_json:
-            data = request.get_json()
-            word = data.get('word')
-            direction = data.get('direction')
-        else:
-            word = request.form.get('word')
-            direction = request.form.get('direction')
 
-        if word is None or direction is None:
-            print("bad word")
-            app.logger.error('Invalid request data')
-            return jsonify({'error': 'Invalid request data'})
+# @app.route('/translate', methods=["POST"])
+# def translate():
+#     """translate input"""
+#     print("start translate")
+#     if not g.user:
+#         print("bad user")
+#         flash("Must sign in first", "danger")
+#         return redirect("/signup")
 
-        if direction == 'en_to_zh':
-            print("en to zh")
+#     try:
+#         print("start try")
+#         if request.is_json:
+#             data = request.get_json()
+#             word = data.get('word')
+#             direction = data.get('direction')
+#         else:
+#             word = request.form.get('word')
+#             direction = request.form.get('direction')
+
+#         if word is None or direction is None:
+#             print("bad word")
+#             app.logger.error('Invalid request data')
+#             return jsonify({'error': 'Invalid request data'})
+
+#         if direction == 'en_to_zh':
+#             print("en to zh")
             
-            detectResponse = translateClient.detect_language(word)
-            print("detect response ***********")
-            print(detectResponse)
-            translateResponse = translateClient.translate(word, 'zh')
-            pinyin = p.get_pinyin(translateResponse['translatedText'], splitter=' ', tone_marks='marks')
-        else:
-            print("zh to en")
-            detectResponse = translateClient.detect_language(word)
-            translateResponse = translateClient.translate(word, 'en')
-            pinyin = p.get_pinyin(word, splitter=' ', tone_marks='marks')
+#             detectResponse = translateClient.detect_language(word)
+#             print("detect response ***********")
+#             print(detectResponse)
+#             translateResponse = translateClient.translate(word, 'zh')
+#             pinyin = p.get_pinyin(translateResponse['translatedText'], splitter=' ', tone_marks='marks')
+#         else:
+#             print("zh to en")
+#             detectResponse = translateClient.detect_language(word)
+#             translateResponse = translateClient.translate(word, 'en')
+#             pinyin = p.get_pinyin(word, splitter=' ', tone_marks='marks')
 
-        word_lang = detectResponse['language']
-        translation_text = translateResponse['translatedText']
-        print("out of if elses")
-        search = Searches(
-            word=word,
-            word_lang=word_lang,
-            translation=translation_text,
-            pinyin=pinyin,
-            user_id=g.user.id
-        )
-        print("write to db")
-        db.session.add(search)
-        db.session.commit()
-        print("write to res")
-        response_data = {
-            'translation': translation_text,
-            'pinyin': pinyin
-        }
-    except Exception as e:
-        app.logger.exception('An error occurred during translation:')
-        return jsonify({'error': str(e)})
-    print("return json res")
-    return jsonify(response_data)
+#         word_lang = detectResponse['language']
+#         translation_text = translateResponse['translatedText']
+#         print("out of if elses")
+#         search = Searches(
+#             word=word,
+#             word_lang=word_lang,
+#             translation=translation_text,
+#             pinyin=pinyin,
+#             user_id=g.user.id
+#         )
+#         print("write to db")
+#         db.session.add(search)
+#         db.session.commit()
+#         print("write to res")
+#         response_data = {
+#             'translation': translation_text,
+#             'pinyin': pinyin
+#         }
+#     except Exception as e:
+#         app.logger.exception('An error occurred during translation:')
+#         return jsonify({'error': str(e)})
+#     print("return json res")
+#     return jsonify(response_data)
 
 
 @app.route('/history')
